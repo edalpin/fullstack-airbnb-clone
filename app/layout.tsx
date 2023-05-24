5637437317dce0af711fe9c29ea4ab15fb1de3d7
Edwin Álvarez Pineda <niwdesk9@gmail.com>
2023-05-24 08:09:27 -0500

    Add register modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
+import ClientOnly from './components/ClientOnly';
 import Modal from './components/modals/Modal';
+import RegisterModal from './components/modals/RegisterModal';
 import Navbar from './components/navbar/Navbar';
 import './globals.css';
 import { Nunito } from 'next/font/google';
+import ToasterProvider from './providers/ToasterProvider';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -18,7 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
